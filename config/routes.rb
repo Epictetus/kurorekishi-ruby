c39ad609777050a0053ec9ef5e728e373daa9393
cohakim <cohakim@gmail.com>
2012-03-12 18:15:33 +0900

    ヘルプページ完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Kurorekishi::Application.routes.draw do
   # root
   resource :roots, :only => [:show]
+  match 'help' => 'roots#help', :path => 'help', :as => 'help'
   match 'oauth_callback' => 'roots#oauth_callback', :path => 'oauth_callback', :as => 'oauth_callback'
+  match 'clean' => 'roots#clean', :path => 'clean', :as => 'clean'
   match 'stats' => 'roots#stats', :path => 'stats', :as => 'stats'
-  post  'clean' => 'roots#clean', :path => 'clean', :as => 'clean'
   match 'reset_session' => 'roots#destroy_session', :path => 'reset_session', :as => 'reset_session'
 
   root :to => "roots#show"
