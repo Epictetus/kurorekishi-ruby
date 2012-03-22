166f6c7b7b82313fe987662aaaa95e81699c9fee
cohakim <cohakim@gmail.com>
2012-03-22 13:36:40 +0900

    テスト環境では.netrcがなくてもエラーを出さないようにした

--- config/initializers/configatron.rb
+++ config/initializers/configatron.rb
@@ -1,6 +1,10 @@
 Configatron::Rails.init
 
-configatron.redis.password          = Net::Netrc.locate('redis').password
-configatron.twitter.consumer_secret = Net::Netrc.locate('kurorekishi_twitter_consumer_secret').password
-configatron.prtools.twitter.consumer_secret     = Net::Netrc.locate('prtools_twitter_consumer_secret').password
-configatron.prtools.twitter.access_token_secret = Net::Netrc.locate('prtools_twitter_access_token_secret').password
+begin
+  configatron.redis.password          = Net::Netrc.locate('redis').password
+  configatron.twitter.consumer_secret = Net::Netrc.locate('kurorekishi_twitter_consumer_secret').password
+  configatron.prtools.twitter.consumer_secret     = Net::Netrc.locate('prtools_twitter_consumer_secret').password
+  configatron.prtools.twitter.access_token_secret = Net::Netrc.locate('prtools_twitter_access_token_secret').password
+rescue => ex
+  raise ex unless Rails.env == 'test'
+end
