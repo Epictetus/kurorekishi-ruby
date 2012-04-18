10fe1273c4dfda43b3c69718c76166a2a27e0012
cohakim <cohakim@gmail.com>
2012-04-18 12:01:39 +0900

    TweetBotのアカウント情報更新

--- config/configatron/production.rb
+++ config/configatron/production.rb
@@ -7,5 +7,5 @@ configatron.twitter.oauth_callback  = { :oauth_callback => 'http://kurorekishi.y
 # PRTools
 configatron.prtools.twitter.consumer_key    = '4upSvEDhihcbIB9VdXBIQ'
 configatron.prtools.twitter.consumer_secret = '<your consumer secret>'
-configatron.prtools.twitter.access_token        = '533074249-0YxG8A0QfsNVB3TJqK45GwfOm1ol7JLpATUIoUKP'
+configatron.prtools.twitter.access_token        = '556482467-AjJwFCopjp8Y45ZV6KcwVoljLe0XT4Sioq6e9IHD'
 configatron.prtools.twitter.access_token_secret = '<your accesstoken secret>'
